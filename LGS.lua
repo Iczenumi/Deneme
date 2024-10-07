55d3bccf413aa98ff73bdfae1cf49c8a8e1157ea
Iczenumi <124516969+Iczenumi@users.noreply.github.com>
2024-10-07 23:01:18 +0300

    LGS.lua

--- LGS.lua
+++ LGS.lua
@@ -16,7 +16,7 @@ local image_1 = {
     ['sizeX'] = 158,
     ['sizeY'] = 158,
     ['paddingX'] = 150, --use this to align it, currently 50 pixels from top right
-    ['paddingY'] = 300 --use this to align it, currently 50 pixels from top right
+    ['paddingY'] = 200 --use this to align it, currently 50 pixels from top right
 }
 
 --this waits for the driver to not be in the setup screen, then starts the timer for the rule splash image
